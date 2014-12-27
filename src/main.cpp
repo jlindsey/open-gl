10f170b277b0538c776711d9d6a8eefe1de6a433
Josh Lindsey <joshua.s.lindsey@gmail.com>
2014-12-27 10:33:15 -0500

    Incremental, messing with submodules

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#define GLEW_STATIC
+#include <GLFW/glfw3.h>
+#include <GL/glew.h>
+#include <thread>
+#include "config.h"
+
+int main(void) {
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+  GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", nullptr, nullptr);
+
+  glfwMakeContextCurrent(window);
+
+  glewExperimental = GL_TRUE;
+  glewInit();
+
+  GLuint verexBuffer;
+  glGenBuffers(1, &verexBuffer);
+
+  while(!glfwWindowShouldClose(window)) {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+
+  return 0;
+}
+
